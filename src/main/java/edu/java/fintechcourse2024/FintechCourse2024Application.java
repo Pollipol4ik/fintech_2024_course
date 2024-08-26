aebcfb94a55ed9d08c633ab872b8a1ac84ed389a
Polina <your_email@whatever.comgit config --global user.name Polinagit config --global user.email your_email@whatever.com>
2024-08-26 14:21:29 +0300

    first commit

---
+++ src/main/java/edu/java/fintechcourse2024/FintechCourse2024Application.java
@@ -0,0 +1,13 @@
+package edu.java.fintechcourse2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechCourse2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintechCourse2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/java/fintechcourse2024/FintechCourse2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.java.fintechcourse2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechCourse2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
